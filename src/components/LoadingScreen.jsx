f97eedaf8c17eca23b004089f2cfb38e3d1ff071
Exploits <seun7674@gmail.com>
2025-08-13 03:05:21 +0100

    name change

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "<AJAYI OLUWASEUN/>";
+  const fullText = "<Ajayi Oluwaseun/>";
 
   useEffect(() => {
     let index = 0;
